e0671de9ce03c71880f5346902196ecd18563e5c
David Mattsson <mattsson.david@gmail.com>
2018-08-24 00:01:04 +0200

    Make a simple monad (Identity) but label it as Maybe for now. Maybe monad will be implemated properly later

--- src/Maybe.mjs
+++ src/Maybe.mjs
@@ -1,4 +1,12 @@
 
 export const Maybe = value => ({
     value,
+    // fmap:: (a -> b) -> f a -> f b
+    fmap: f => Maybe(f(value)),
+    // apply:: f (a -> b) -> f a -> f b
+    apply: fA =>  Maybe(value(fA.value)),
+    // bind:: m a -> (a -> m b) -> m b    operator:>>=
+    flatMap: aToMonad => Maybe(aToMonad(value).value),
 });
+// return:: a -> f a
+Maybe.return = value => Maybe(value);
--- src/Maybe.test.mjs
+++ src/Maybe.test.mjs
@@ -7,8 +7,34 @@ const expect = chai.expect;
 import {Maybe} from './Maybe';
 
 mocha.describe('Maybe', () => {
-    mocha.it('can wrap a value', () => {
+    mocha.it('wraps a value', () => {
         const maybe = Maybe(1);
         expect(maybe.value).to.equal(1);
-    })
+    });
+
+    mocha.it('returns', () => {
+        const maybe = Maybe.return(1);
+        expect(maybe.value).to.equal(1);
+    });
+
+    mocha.it('fmaps', () => {
+        const mapped = Maybe.return(1).fmap(n => n + 1);
+        expect(mapped.value).to.equal(2);
+    });
+
+    mocha.it('applies', () => {
+        const addOne = Maybe.return(n => n + 1);
+        expect(addOne.apply(Maybe.return(1)).value).to.equal(2);
+        const addOneOne = Maybe.return(n => m => n + m + 1);
+        expect(addOneOne.apply(Maybe(1)).apply(Maybe(2)).value).to.equal(4);
+    });
+
+    mocha.it('binds aka. flatMaps', () => {
+        const maybe = Maybe.return(1).flatMap(n => Maybe(n + 1));
+        expect(maybe.value).to.equal(2);
+        const chained = Maybe.return(1)
+            .flatMap(n => Maybe(n + 1))
+            .flatMap(n => Maybe(n * n));
+        expect(chained.value).to.equal(4);
+    });
 });
