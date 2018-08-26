2aa6f9ac9c8ae9a8101a6f2ad8074f1568743f94
David Mattsson <mattsson.david@gmail.com>
2018-08-26 03:29:00 +0200

    Update the Maybe monad

--- src/Maybe.mjs
+++ src/Maybe.mjs
@@ -1,12 +1,48 @@
 
-export const Maybe = value => ({
-    value,
+// data Maybe a = Just a | Nothing
+export const Maybe = value => isNothing(value) ? Nothing() : Just(value);
+// return:: a -> f a
+Maybe.return = value => Maybe(value);
+
+const isNothing = value => value === null || typeof value === 'undefined';
+
+const Just = value => ({
     // fmap:: (a -> b) -> f a -> f b
     fmap: f => Maybe(f(value)),
     // apply:: f (a -> b) -> f a -> f b
-    apply: fA =>  Maybe(value(fA.value)),
+    apply: fA =>  {
+        if (typeof fA.flatten !== 'function') {
+            return Nothing();
+        }
+        if (typeof fA.isNothing === 'function' && fA.isNothing() === true) {
+            return Nothing();
+        }
+        return Maybe(value(fA.flatten()))
+    },
     // bind:: m a -> (a -> m b) -> m b    operator:>>=
-    flatMap: aToMonad => Maybe(aToMonad(value).value),
+    flatMap: aToMonad => {
+        const monadicB = aToMonad(value);
+        if (typeof monadicB.isNothing === 'function'
+            && monadicB.isNothing() === true
+        ) {
+            return Nothing();
+        }
+        if (typeof monadicB.flatten !== 'function') {
+            return Nothing();
+        }
+        return Maybe(monadicB.flatten())
+    },
+    // flatten:: m a -> a
+    flatten: _ => value,
+    isNothing: _ => false,
 });
-// return:: a -> f a
-Maybe.return = value => Maybe(value);
+
+const Nothing = () => ({
+    fmap: _ => Nothing(),
+    apply: _ => Nothing(),
+    flatMap: _ => Nothing(),
+    flatten: _ => {
+        throw new Error('Nothing has no value');
+    },
+    isNothing: _ => true,
+});
--- src/Maybe.test.mjs
+++ src/Maybe.test.mjs
@@ -8,33 +8,135 @@ import {Maybe} from './Maybe';
 
 mocha.describe('Maybe', () => {
     mocha.it('wraps a value', () => {
-        const maybe = Maybe(1);
-        expect(maybe.value).to.equal(1);
+        expect(Maybe(1).flatten()).to.equal(1);
+
+        expect(Maybe(1).isNothing()).to.equal(false);
+
+        expect(Maybe(null).isNothing()).to.equal(true);
     });
 
     mocha.it('returns', () => {
-        const maybe = Maybe.return(1);
-        expect(maybe.value).to.equal(1);
+        expect(Maybe.return(1).flatten()).to.equal(1);
     });
 
     mocha.it('fmaps', () => {
-        const mapped = Maybe.return(1).fmap(n => n + 1);
-        expect(mapped.value).to.equal(2);
+        expect(
+            Maybe.return(1)
+                .fmap(n => n + 1)
+                .flatten()
+        ).to.equal(2);
+
+        expect(
+            Maybe.return(1)
+                .fmap(n => n + 1)
+                .fmap(n => n * 2)
+                .flatten()
+        ).to.equal(4);
+
+        expect(
+            Maybe(0)
+                .fmap(n => (n > 0) ? n : null)
+                .isNothing()
+        ).to.equal(true);
+
+        expect(
+            Maybe(6)
+                .fmap(n => n / 2)
+                .fmap(n => n / 2)
+                .fmap(n => (n > 0) ? n : null)
+                .isNothing()
+        ).to.equal(false);
     });
 
     mocha.it('applies', () => {
-        const addOne = Maybe.return(n => n + 1);
-        expect(addOne.apply(Maybe.return(1)).value).to.equal(2);
-        const addOneOne = Maybe.return(n => m => n + m + 1);
-        expect(addOneOne.apply(Maybe(1)).apply(Maybe(2)).value).to.equal(4);
+        expect(
+            Maybe.return(n => n + 1)
+                .apply(Maybe.return(1))
+                .flatten()
+            ).to.equal(2);
+
+        expect(
+            Maybe.return(n => m => n + m + 1)
+                .apply(Maybe(1))
+                .apply(Maybe(2))
+                .flatten()
+        ).to.equal(4);
+
+        expect(
+            Maybe(n => m => n + m)
+                .apply(Maybe(1))
+                .apply(Maybe(null))
+                .isNothing()
+        ).to.equal(true)
+
+        expect(
+            Maybe(n => m => n + m)
+                .apply(Maybe(1))
+                .apply(Maybe(null))
+                .apply(Maybe(2))
+                .isNothing()
+        ).to.equal(true)
+
+        expect(
+            Maybe(n => m => n + m)
+                .apply(Maybe(1))
+                .apply(Maybe(2))
+                .isNothing()
+        ).to.equal(false)
     });
 
     mocha.it('binds aka. flatMaps', () => {
-        const maybe = Maybe.return(1).flatMap(n => Maybe(n + 1));
-        expect(maybe.value).to.equal(2);
-        const chained = Maybe.return(1)
-            .flatMap(n => Maybe(n + 1))
-            .flatMap(n => Maybe(n * n));
-        expect(chained.value).to.equal(4);
+        expect(
+            Maybe.return(1)
+                .flatMap(n => Maybe(n + 1))
+                .flatten()
+            ).to.equal(2);
+
+        expect(
+            Maybe.return(1)
+                .flatMap(n => Maybe(n + 1))
+                .flatMap(n => Maybe(n * n))
+                .flatten()
+        ).to.equal(4);
+
+        expect(
+            Maybe.return(1)
+                .flatMap(_ => Maybe(null))
+                .isNothing()
+        ).to.equal(true);
+
+        expect(
+            Maybe.return(null)
+                .flatMap(_ => Maybe(null))
+                .isNothing() // Nothing in, nothing done.. should be Nothing out
+        ).to.equal(true);
+
+        expect(
+            Maybe.return(1)
+                .flatMap(_ => Maybe(1))
+                .flatMap(_ => Maybe(null))
+                .flatMap(_ => Maybe(2))
+                .isNothing()
+        ).to.equal(true);
+
+        const hasValue = Maybe(1)
+            .flatMap(_ => Maybe(2))
+            .flatMap(n => Maybe(n + 1));
+        expect(hasValue.isNothing()).to.equal(false);
+        expect(hasValue.flatten()).to.equal(3);
+
+        const isEven = n => n % 2 === 0;
+        const halfIt = n => {
+            return isEven(n)
+                ? Maybe(n / 2) // Just a
+                : Maybe(null); // Nothing
+        };
+        expect(
+            Maybe(20)
+                .flatMap(halfIt) // Just 20 -> Just 10
+                .flatMap(halfIt) // Just 10 -> Just 5
+                .flatMap(halfIt) // Just 5 -> Nothing
+                .isNothing()
+        ).to.equal(true);
     });
 });
