0aefa569a6765bf5dc12854c3d73b1f73e1df465
David Mattsson <mattsson.david@gmail.com>
2018-08-30 23:33:21 +0200

    WIP: Add State monad

--- src/State.mjs
+++ src/State.mjs
@@ -1,5 +1,25 @@
+/*
+ * State Monad
+ */
 
 export const State = stateFn => ({
+    // fmap:: (a -> b) -> State s a -> State s b
+    // fmap:: (a -> b) -> (s -> (a, s)) -> (s -> (b, s))
+    fmap: f => State(state => {
+        const unwrappedValueState = stateFn(state);
+        return {value: f(unwrappedValueState.value), state: unwrappedValueState.state};
+    }),
+    // apply:: State s (a -> b) -> State s a -> State s b
+    // apply:: (s -> ((a -> b), s)) -> (s -> (a, s)) -> (s -> (b, s))
+    apply: otherState => State(state => {
+        // Assume `this state`'s value is a function
+        const thisNewValueState = stateFn(state);
+        const otherNewValueState = otherState.runState(thisNewValueState.state);
+        return {
+            value: thisNewValueState.value(otherNewValueState.value),
+            state: otherNewValueState.state
+        };
+    }),
     // bind:: State s a -> (a -> State s b) -> State s b    operator:>>=
     // bind:: (s -> (a, s)) -> (a -> s -> (b, s)) -> (s -> (b, s))
     flatMap: aToState => {
@@ -11,6 +31,8 @@ export const State = stateFn => ({
     },
     // runState:: s -> (a, s)  # runState unwraps the function in the State context
     runState: state => stateFn(state),
+
+    // ToDo:: Implement put & get
     // put:: s -> State s ()
     // put:: s -> (_ -> ((), s)
     put: state => State(_ => ({value: null, state: state})),
@@ -20,12 +42,4 @@ export const State = stateFn => ({
 });
 // return:: a -> State s a  !! State s a ::aka (s -> (a, s))
 // return:: a -> (s -> (a, s))
-State.return = value => State(state => ({value: value, state: state}));
-
-console.log(State.return(1).runState(2));
-console.log(
-    State.return(1)
-        .flatMap(n => State(state => ({value: n + 1, state: state})))
-        .flatMap(n => State(state => ({value: n + 1, state: state + 1})))
-    .runState(2)
-);
+State.return = value => State(state => ({value, state}));
--- src/State.test.mjs
+++ src/State.test.mjs
@@ -8,17 +8,74 @@ import {State} from './State';
 
 mocha.describe('State', () => {
     mocha.it('wraps a value', () => {
+        const makeState = value => State(state => ({value, state}));
+        const aState = makeState(1);
+        const theState = aState.runState(2);
+        expect(theState.value).to.equal(1);
+        expect(theState.state).to.equal(2);
     });
 
     mocha.it('returns', () => {
+        const state = State.return(1).runState(2);
+        expect(state.value).to.equal(1);
+        expect(state.state).to.equal(2);
     });
 
     mocha.it('fmaps', () => {
+        const state = State.return(1)
+            .fmap(n => n + 1)
+            .runState(2);
+        expect(state.value).to.equal(2);
+        expect(state.state).to.equal(2);
+
+        const multi = State.return(1)
+            .fmap(n => n + 1)
+            .fmap(n => n * 2)
+            .runState(2);
+        expect(multi.value).to.equal(4);
+        expect(multi.state).to.equal(2);
     });
 
     mocha.it('applies', () => {
+        const simple = State.return(n => n + 1)
+            .apply(State.return(1))
+            .runState(2);
+        expect(simple.value).to.equal(2);
+        expect(simple.state).to.equal(2);
+
+        const multi = State.return(a => b => a + b)
+            .apply(State.return(1))
+            .apply(State.return(2))
+            .runState(2);
+        expect(multi.value).to.equal(3);
+        expect(multi.state).to.equal(2);
+
+        const mutateValueAndState = State.return(n => n + 1)
+            .apply(State(state => ({value: 1, state: state + 1})))
+            .runState(2);
+        expect(mutateValueAndState.value).to.equal(2);
+        expect(mutateValueAndState.state).to.equal(3);
     });
 
     mocha.it('binds aka. flatMaps', () => {
+        const state = State.return(1)
+            .flatMap(n => State(state => ({value: n + 1, state})))
+            .flatMap(n => State(state => ({value: n + 1, state})))
+            .runState(2);
+        expect(state.value).to.equal(3);
+        expect(state.state).to.equal(2);
+
+        const mutateValueAndState = State.return(1)
+            .flatMap(n => State(state => ({value: n + 1, state})))
+            .flatMap(n => State(state => ({value: n + 1, state: state + 1})))
+            .runState(2);
+        expect(mutateValueAndState.value).to.equal(3);
+        expect(mutateValueAndState.state).to.equal(3);
+    });
+
+    mocha.it('puts', () => {
+    });
+
+    mocha.it('gets', () => {
     });
 });
